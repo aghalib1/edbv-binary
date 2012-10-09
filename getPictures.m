e8fac6e9e095b149cf664e8f05cf4a68ccc199aa
manuelkroeter <manuelkroeter@gmail.com>
2012-10-09 15:09:59 +0000

    initial commit

---
+++ getPictures.m
@@ -0,0 +1,25 @@
+% Gruppennummer AG_A_5
+% Höller Benjamin 0925688
+
+
+function [ A ] =getPictures()
+    A = cell(3,2); 
+    %in diesem Cell Array die Bilddaten Gespeichert:
+    % 1 Bild | 2 Dateiname des Bildes
+    
+    disp('Bilder werden eingelesen ...')
+    D = dir('pictures'); %erzeugt eine Liste aller Datein des Ordners
+    j = 1 ;    %Zähler für die Bilder
+    
+    %Diese Schleife speichert alle in pictures befindlichen bilder in A
+    for i = 1 : size( D, 1 )
+       if D( i ).isdir == 0 %hier bildeinschränkungen möglich
+            A{j,1} = imread (strcat('pictures/',D( i ).name)) ;
+            A{j,2} = [D( i ).name ];
+            j = j +1;
+        end
+        
+    end
+
+end
+
