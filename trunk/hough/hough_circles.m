bf64f1d2cbe71a792905716771624b9c968c0b8c
manuelkroeter@gmail.com <manuelkroeter@gmail.com@55b71cb5-671a-7388-9f1c-aaeb44f028d2>
2012-10-19 08:33:56 +0000

    hough improvements

--- trunk/hough/hough_circles.m
+++ trunk/hough/hough_circles.m
@@ -1,45 +1,27 @@
 %Author: Manuel Kröter
 
-function [ final_points ] = hough_circles(img)
-%function [ final_points ] = hough_circles img, scale_start, scale_end, min_pixels, max_pixels )
+function [ final_points ] = hough_circles(img, radius_x, radius_y)
+% Hough Transformation to detect zeros
 %
-% THIS INFO HAS TO BE UPDATED!
-%
-%First try with Hough Transformation to detect zeros!
-%Simplest version, does not consider connectivity of points!!
-%Its hard to detect horizontal lines without using connectivity
-%There are always a lot of pixels in a line of text horizontally...
-
 %Input: 
-%- aleady thinned black white image
-%- start + end angles in degrees, start < end (angle of the corresponding normal!!)
-%- minimum and maximum pixels on the line, when there are more or less pixels, the corresponding line is not detected
+%- aleady thinned black white image, foreground is white
+%- radius_x: radius of the ellipse/zero (to be detected) in x-direction
+%- radius_y: radius of the ellipse/zero (to be detected) in y-direction
+
+%  The radius values depend on font style and size
+%  Good values for Arial, Size 20, are:
+%  radius_x = 4.8;
+%  radius_y = 8;
+%  UPDATE THIS INFO WHEN YOU FOUND BETTER VALUES!!
 
 %Output:
-%- Matrix containing x and y points of detected lines
-%- Rows = Points
-%- 1.Column x, 2.Column y 
+%- Matrix containing x and y points (=center of found zeros)
+%  Rows = Points
+%  1.Column x, 2.Column y 
 
-%ATTENTION: The returned points MAY NOT LIE on the actual line segment!
-%Die gelieferten Punkte sind Lotfußpunkte der Normalen (der gesuchten Geraden), welche zum Ursprung zeigt
-
-
-
-%     if angle_end<=angle_start
-%         disp 'Start angle has to be bigger than end angle';
-%        return; 
-%     end
-%     if min_pixels>=max_pixels || min_pixels<0 || max_pixels<0
-%         disp 'Error. Wrong parameter values.';
-%        return; 
-%     end
-    
- %   total_degrees = angle_end-angle_start+1;
 
     height = size(img,1);
     width = size(img,2);
-    
-    scale = 8;
 
     houghRaum = zeros(size(img));
     
@@ -53,8 +35,8 @@ function [ final_points ] = hough_circles(img)
                 for t=1:5:360
                     
                     degreeCounter = degreeCounter + 1;
-                    x_ellipse = round(x+scale*0.6*cosd(t));
-                    y_ellipse = round(y+scale*1*sind(t));
+                    x_ellipse = round(x+radius_x*cosd(t));
+                    y_ellipse = round(y+radius_y*sind(t));
                         
                     if x_ellipse>0 && y_ellipse>0 && x_ellipse<=width && y_ellipse<=height
                         houghRaum(y_ellipse,x_ellipse) = houghRaum(y_ellipse,x_ellipse) + 1;
@@ -67,13 +49,37 @@ function [ final_points ] = hough_circles(img)
     
     m = max(houghRaum(:));  %could also be a user input (how many pixels have to be on the ellipse to be considered as a result ellipse)
     
+    %Comment the next two lines when using the function in final script
     %scale the values to 0-1 to show the result as an image
     scaledHough = double(houghRaum./m);
     imshow(scaledHough);
     
-    temp = houghRaum;
-    temp(temp<m)=0; %perhaps a litte buffer zone
-    [y x] = find(temp);
+    %Which ellipse centers should be returned as results?
+    %Now: Only the centers of those ellipses with the maximum amount of
+    %pixels on it. So if there is a ellipse with only one pixel less, the
+    %corresponding center is not returned.
+    houghRaum(houghRaum<m)=0;
+    [y x] = find(houghRaum);
+    
+    
+    %TEMPORARY, Should be improved! When changed, also update the version for line detection!
+    %merge nearby points, maybe use average pixel of all nearby pixels
+    %now just the first pixel is chosen
+    points = [x y];
+    final_points = points(1,:);
+    for i=2:size(points,1)
+        insert = 1;
+        for j=1:size(final_points,1)
+            if abs(sqrt(final_points(j,1)^2+final_points(j,2)^2)-sqrt(points(i,1)^2+points(i,2)^2))<3 %which threshold distance?
+                insert = 0;
+            end
+        end
+        if insert == 1
+            final_points(j+1,:)=points(i,:);
+        end
+    end
+    
+    
     final_points=[x y];
 
 end
