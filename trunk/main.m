964f82c22bda2908298b050a3f0ec770ae0da68a
manuelkroeter@gmail.com <manuelkroeter@gmail.com@55b71cb5-671a-7388-9f1c-aaeb44f028d2>
2012-11-01 18:35:03 +0000

    detection with chain code works now

--- trunk/main.m
+++ trunk/main.m
@@ -12,9 +12,13 @@ clc;
 disp('Bilder einlesen')
 A=getPictures;
 %jedes Bild analysieren
+
+
 for i = 1 : size( A, 1 )
+  
     if ~isempty(A(i,1))
     figure;
     analyze(A(i,1),A(i,2)); 
     end
+    
 end
