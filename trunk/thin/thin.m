e0214e11151b106d0e48489ca5a9cde74921eed0
serj.warumono@gmail.com <serj.warumono@gmail.com@55b71cb5-671a-7388-9f1c-aaeb44f028d2>
2012-12-03 11:13:52 +0000

    thin.m working.

--- trunk/thin/thin.m
+++ trunk/thin/thin.m
@@ -1,129 +1,127 @@
-% Gruppennummer AG_A_5
-% Wiplinger Sascha (0702060)
+%Manuel Kröter
+%Sascha Wiplinger
+%
+%Version: 3.12.2012
+%Function thin
+%
+%Skeletizes a black-white-image.
+%Input: a black and white image; white == foreground, black == background
+%
+%Output: the thinned black and white picture.
+
+function [ thinned ] = thin_test( img )
 
 
-%assumption: 1 means 'white', 0 means 'black'
-%precondition: picture has at least 3 rows and 3 columns and is a
-    %rectangular matrix.
-%precondition: the only values contained are 1 (white) and 0 (black).
-%precondition: the border is completely white (haven't checked for this 
-    %'special case', but inserting a white border should not be a big 
-    %problem, i guess)
-%postcondition: the black areas will be thinned to curves which have the
-    %width of 1 pixel.
-%postcondition: the resulting curves are 8-joined.
+thinned = img;
+width = size(img,2);
+height = size(img,1);
 
-function [thinnedPicture] = thin(BWPicture) %input: 2D-Matrix (values 1 or 0), return: matrix with thinned picture (also values 1 and 0)
+S1 = [-1,-1,-1;
+       0,1,0;
+       1,1,1];
+S2 = [-1,-1,0;
+      -1,1,1;
+       0,1,1]; 
 
-thinnedPicture = (BWPicture-1)*-1; %invert pic, so 0 means background and 1 means foreground (easier for calculation of connected components)
-picSize = size(thinnedPicture);
+P1 =  [-1,-1,-1;
+       -1, 1,-1;
+       -1, 0, 0];
+   
+P2 =  [-1,-1,-1;
+       -1, 1,-1;
+        0, 0,-1];  
+   
 
-
-%for i=0:3
-%starting in the upper left corner
-thinnedPictureLeftToBottom=thinnedPicture;
-row=1;
-col=1;
-%count=1;
-while(row+2<=picSize(1) )
-    while(col+2<=picSize(2))
-       if  thinnedPictureLeftToBottom(row+1,col+1) == 1
-           kernel = thinnedPictureLeftToBottom(row:row+2,col:col+2);
-           thinnedPictureLeftToBottom(row+1,col+1)=1-deleteable(kernel);
-           %debug-stuff
-           %if count <= 1
-            %   kernel
-            %   [value, num1, num2, neighbour] = deleteable(kernel);
-            %   if value==0
-            %       num1
-            %       num2
-            %       neighbour
-            %   end
-            %   count = count+1;
-           %end
-       end
-       col=col+1;
-    end
-    %count=1;
-    col=1;
-    row=row+1;
-end
-
-%starting in the upper right corner
-thinnedPictureRightToBottom = thinnedPicture;
-row=1;
-col=picSize(2);
-while(row+2<=picSize(1) )
-    while(col>=3)
-       if  thinnedPictureRightToBottom(row+1,col-1) == 1
-           kernel = thinnedPictureRightToBottom(row:row+2,col-2:col);
-           thinnedPictureRightToBottom(row+1,col-1)=1-deleteable(kernel);
-       end
-       col=col-1;
-    end
-    col=picSize(2);
-    row=row+1;
-end
-
-%thinnedPictureLeftToUpper=thinnedPicture;
-%row=picSize(1);
-%col=1;
-%while(row>=3 )
-%    while(col+2<=picSize(2))
-%       if  thinnedPictureLeftToUpper(row-1,col+1) == 1
-%           kernel = thinnedPictureLeftToUpper(row-2:row,col:col+2);
-%           thinnedPictureLeftToUpper(row-1,col+1)=1-deleteable(kernel);
-%       end
-%       col=col+1;
-%    end
-%    col=1;
-%    row=row-1;
-%end
-
-%thinnedPictureRightToUpper = thinnedPicture;
-%row=picSize(1);
-%col=picSize(2);
-%while(row>=3 )
-%    while(col>=3)
-%       if  thinnedPictureRightToUpper(row-1,col-1) == 1
-%           kernel = thinnedPictureRightToUpper(row-2:row,col-2:col);
-%           thinnedPictureRightToUpper(row-1,col-1)=1-deleteable(kernel);
-%       end
-%       col=col-1;
-%    end
-%    col=picSize(2);
-%    row=row-1;
-%end
-
-thinnedPicture = (thinnedPictureLeftToBottom );%& thinnedPictureRightToBottom);% | (thinnedPictureRightToUpper & thinnedPictureRightToBottom);
-thinnedPicture = (thinnedPicture-1).*-1; %returned to 1 for white and 0 for black
-end
-
-%checks if it is OK to delete the pixel.
-%postcondition: returns 1 if it is OK, 0 if it is not OK.
-function [value, num1, num2, neighbour] = deleteable(kernel)
-    value=0;
-    testKernel = kernel;
-    testKernel(2,2)=0; %Kernel with center Pixel set to 0
+%While loop control
+cc = 0; 
+changed = 1;
+while changed && cc < 20;
     
-    %the pixel can be deleted if it does not keep together a componant and
-    %if it is not an end pixel.
-    %a pixel is an end pixel if it has 0 or 1 neighbours.
-    [L1, num1] = bwlabel(kernel, 4); 
-    [L2, num2] = bwlabel(testKernel, 4); %have to write that myself yet
+    changed = 0;
+    cc = cc + 1;
+
+    for i=1:4
+        markMatrix = bwmatch(thinned,S1);
+        if sum(markMatrix(:)) > 0
+            changed = 1;
+        end
+        thinned(markMatrix > 0) = 0;
+        S1 = rot90(S1);
+    end
     
-    neighbour=0;
-    for x=1 : 9
-        neighbour=neighbour+testKernel(x);
+    
+    for i=1:4
+        markMatrix = bwmatch(thinned,S2);
+        if sum(markMatrix(:)) > 0
+            changed = 1;
+        end
+        thinned(markMatrix > 0) = 0;
+        S2 = rot90(S2);
     end
-    if num1 == num2 && neighbour>1
-        value=1;
-    end
+
 end
 
-%looks for 4-connected components in the kernel
-function [] = CCL4 (kernel)
-    
 
+%Pruning
+
+for i=1:1
+
+    
+   for j=1:4
+     markMatrix = bwmatch(thinned,P1);
+     thinned(markMatrix > 0) = 0;
+     P1 = rot90(P1);
+       
+   end
+   
+   for j=1:4
+     markMatrix = bwmatch(thinned,P2);
+     thinned(markMatrix > 0) = 0;
+     P2 = rot90(P2);
+       
+   end
+   
+  
+end
+
+
+end
+
+% Function bwmatch
+%
+% returns a matrix containing 1 for pixels that can be removed and 0 for
+% those which have to be kept.
+%
+% Input:
+% bw1: black-white-image
+% kernel: the kernel for matching.
+%   1: there has to be a 1.
+%  -1: there has to be a 0.
+%   0: don't care.
+%
+% Output:
+% the matrix marking the deletable pixels with 1.
+
+function [bw2] = bwmatch(bw1, kernel)
+
+bw2 = false(size(bw1)); %matrix mit groesze von bw1, aber mit 0 gefuellt.
+s = size(bw1);
+for j= 1:s(2)
+    for i = 1:s(1)
+        if(bw1(i,j)==1) %run-time-optimization
+            sub = bw1( max(1,i-1):min(i+1,s(1)), max(1,j-1):min(j+1,s(2)) ); %sub-matrix of the picture being tested
+            
+            %comparision of the subset with the kernel. only if the pixel is
+            %not on the border.
+            if(size(sub)==size(kernel))
+                if (sub(kernel==-1) == 0) %2 ifs, because && only works for singular true/false
+                    if (sub(kernel==1)==1)
+                        bw2(i,j)=1;
+                    end
+                end
+            end
+        end
+    end
+end
 
 end
