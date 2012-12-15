237c774addf5be09e62d16ddd9cbd84422141c1f
philipp.schiffner@gmail.com <philipp.schiffner@gmail.com@55b71cb5-671a-7388-9f1c-aaeb44f028d2>
2012-12-15 15:23:25 +0000

    im2bw geändert

--- trunk/bw/bw.m
+++ trunk/bw/bw.m
@@ -1,13 +1,13 @@
 function [ bwimg ] = bw (img, thresh, bg)
 
-    bwimg = ones(size(img));
-    s = size(img);
-    for y = 1:s(2)
-        for x = 1:s(1)
-            if( img(x,y) < thresh)
-                bwimg(x,y,1)=0;
-                bwimg(x,y,2)=0;
-                bwimg(x,y,3)=0;
+    bwimg = ones(size(img,1),size(img,2));
+    for y = 1:size(img,2)
+        for x = 1:size(img,1)
+            r=0.299*img(x,y,1);
+            g=0.587*img(x,y,2);
+            b=0.114*img(x,y,3);
+            if( r+b+g <= (thresh*255))
+                bwimg(x,y)=0;
             end
         end
     end
