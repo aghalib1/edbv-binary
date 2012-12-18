368a063052cfda5a7fa682dc5a7308622eb75e17
benjaminhoeller <benjaminhoeller@gmx.at>
2012-12-18 12:22:12 +0000

    x.m zu test.m umbenannt und funktionalität sichergestellt. (betrifft nur graythresh)

--- graythresh/test.m
+++ graythresh/test.m
@@ -2,19 +2,19 @@
 %Version: 8.11.2012
 
 
-function [ output_args ] = x( input_args )
-%X Testfunktion for simpleGrayThresh
-%   Detailed explanation goes here
+function [ output_args ] = test( input_args )
+%X Testfunction for simpleGrayThresh
+%   runs the standart matlab graythresh function and the
+%   simpleGrayThres funktion for each picture. 
 close All;
 cd ../
-A=getPictures; %add apropriate pictures for testing
+A=getPictures(1); %add apropriate pictures for testing
 cd graythresh
 for i = 1 : size( A, 1 )
   p=A(i,1);
     if ~isempty(p)
       I(1)=  graythresh(p{1});
     
-       
      I(2)=  simpleGrayThresh(p{1});
     disp([A(i,2),I(1),I(2)]);
      
