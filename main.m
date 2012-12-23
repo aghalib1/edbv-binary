2abcb602b779f2eff72aec92d987f55af5cbc73a
philipp.schiffner <philipp.schiffner@gmail.com>
2012-12-23 21:32:03 +0000

    main.m test auf 0 gesetzt

--- main.m
+++ main.m
@@ -17,7 +17,7 @@ clc;
 
 %if testmode is active, the folder testPictures will be read
 %else the normal pictures Folder
-test=1;
+test = 0;
 A=getPictures(test);
 
 %use chain code (= 1) or hough detection (= 0) ?
